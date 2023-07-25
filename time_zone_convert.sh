1d0c8b9279dd5461c57c0f049e8dc866d2af8e31
rahul-wayne <58177891+rahul-wayne@users.noreply.github.com>
2023-07-25 18:55:29 +0530

    Add files via upload
    Time zone converter using bash

---
+++ time_zone_convert.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#echo "Daylight saving time (DST) is the practice of advancing clocks during summer months by one hour"
+#echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#echo "Central Daylight Time (CDT): Second Sunday in March to the first Sunday in November"
+#echo "Central Standard Time (CST): Remainder of the year"
+#echo "------------------------------------------------------------------------------------"
+#echo "Eastern Daylight Time (EDT): Second Sunday in March to the first Sunday in November"
+#echo "Eastern Standard Time (EST): Remainder of the year"
+#echo "------------------------------------------------------------------------------------"
+echo -e "\n"
+val=0
+current_time=$(date "+%Y-%m-%d %H:%M %Z")
+echo Current System time is $current_time 
+echo -e "\n"
+while [[ $val == 0 ]]; do
+read -r -p "Enter the date and time in YYYY-MM-DD HH:MM (24Hr) [TIME_ZONE] format eg: 2023-07-24 14:00 IST: " inp
+#echo -e "\n"
+if [ -n "$inp" ] 
+then
+BST=`TZ="Europe/London" date -d "$inp" "+%Y-%m-%d %H:%M %Z"`
+#echo $BST
+#echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+#echo "Given time $inp in different Time Zones"
+#echo "********************************************************"
+#TZ="Europe/London" date -d "$inp" "+%Y-%m-%d %H:%M %Z"
+#TZ="UTC" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+#TZ="America/Costa_Rica" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+#TZ="America/Chicago" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+#TZ="EST" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+#TZ="America/New_York" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+#TZ="Asia/Kolkata" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+if [[ $? -ne 0 ]]
+then
+  echo "**** ERROR :: INVALID DATE ****"
+else
+  val=1
+  CST=`TZ="America/Costa_Rica" date -d "$BST" "+%Y-%m-%d %H:%M %Z" | awk {'print $NF'}`
+  CDT=`TZ="America/Chicago" date -d "$BST" "+%Y-%m-%d %H:%M %Z" | awk {'print $NF'}`
+  EST=`TZ="EST" date -d "$BST" "+%Y-%m-%d %H:%M %Z"  | awk {'print $NF'}`
+  EDT=`TZ="America/New_York" date -d "$BST" "+%Y-%m-%d %H:%M %Z"  | awk {'print $NF'}`
+  echo -e "\n"
+  echo "########################################################"
+  if [[ "$CST" = "$CDT" && "$EST" = "$EDT" ]]; then
+    echo "xxxxxxxxxxxx NOT IN DAYLIGHT SAVING TIME xxxxxxxxxxxx"
+    TZ="UTC" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="EST" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="America/Costa_Rica" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="Asia/Kolkata" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+  else
+    echo "************ DAYLIGHT SAVING TIME (DST) *************"
+    TZ="UTC" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="America/New_York" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="America/Chicago" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+    TZ="Asia/Kolkata" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
+  fi
+  echo "########################################################"
+  echo "Daylight Saving Time (DST) is the practice of advancing clocks during summer months by one hour"
+  echo  DST - \"From Second Sunday in March to the First Sunday in November\"
+fi
+else
+ val=0
+fi
+done
+
