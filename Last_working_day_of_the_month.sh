92af0cfb87aa6a41318c5712f23c7639ef20096d
rahul-wayne <58177891+rahul-wayne@users.noreply.github.com>
2023-07-25 23:56:42 +0530

    Create Last_working_day_of_the_month.sh
    Last working day of the month

---
+++ Last_working_day_of_the_month.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+var1=`date +%d`
+var2=`cal | awk '{ if(NF>1) a=$NF ; if (NF==7) a=$6}END{print a}'`
+echo $var1
+echo $var2
+
+if [[ $var1 -eq $var2 ]]
+then
+  /bin/sh /home/test2/test2.sh
+else
+  echo "both are not equal"
+fi
