de44f5a470a8d4ef41819bb10b8a8bb65537dd24
rahul-wayne <58177891+rahul-wayne@users.noreply.github.com>
2023-08-01 14:41:06 +0530

    Update ssh-uidv2.sh

--- ssh-uidv2.sh
+++ ssh-uidv2.sh
@@ -3,7 +3,7 @@ USERNAME=`whoami`
 #USERNAME=root
 read -r -p "Please enter USER ID: " user_id
 read -r -p "Enter full path of ip list:" ip_list
-read -r -s -p "Please enter your password for the user $USERNAME: " pass
+read -r -s -p "Please enter password for the user $USERNAME: " pass
 echo "USER ID = $user_id"
 cmd_var="grep -E '$user_id' /etc/passwd"
 for HOSTNAME in `cat $ip_list` ; do
