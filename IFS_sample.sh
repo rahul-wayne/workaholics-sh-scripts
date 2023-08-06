581e71e2953fc246fa0be4ea7276b082d73bc0a3
gigithmg1984 <66333702+gigithmg1984@users.noreply.github.com>
2023-08-06 19:22:57 +0530

    Create IFS_sample.sh
    IFS reference

---
+++ IFS_sample.sh
@@ -0,0 +1,13 @@
+# To overcome spaces in keywords. 
+# new line is taken as Internal Field Separator (IFS) instead of space
+
+# 1. Save old $IFS
+oldIFS="$IFS"
+
+# 2. Now set up a new value to :
+IFS="\n"
+
+for i in `cat ref_file`; do echo "$i"; done
+
+# 3. Restore $IFS
+IFS="$oldIFS"
