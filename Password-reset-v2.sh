c049021c94bb5265708a2f804411fef9bccfd778
rahul-wayne <58177891+rahul-wayne@users.noreply.github.com>
2023-09-02 22:27:01 +0530

    Create Password-reset-v2.sh

---
+++ Password-reset-v2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERNAME=root
+iplist=all_server_list
+BC=$'\033[1;31m'
+EC=$'\033[0m'
+BCG=$'\033[1;32m'
+var=1
+while [ "$var" = 1 ];do
+read -r -p "Please enter USER NAME: " user_name
+if [[ -z "$user_name" ]];then
+   echo  "${BC}User name cannot be empty!${EC}"
+   var=1
+elif [[ "$user_name" == "root" ]];then
+   echo "${BC}This is not permitted you cannot reset the password of root!${EC}"
+else
+var=0
+fi
+done
+var=1
+while [ "$var" = 1 ];do
+read -r -s -p "Please enter PASSWORD: " pssword
+echo -e "\n"
+if [[ -z "$pssword" ]];then
+   echo "${BC}Password cannot be empty!${EC}"
+   var=1;
+else
+var=0
+fi
+done
+if [[ -z "$user_name" || "$user_name" == "root" || -z "$pssword" ]]; then
+    echo "This is not permitted"
+    exit
+else
+for i in `cat $iplist`;do
+ssh -l ${USERNAME} $i 'bash -s' << EOF
+if getent passwd $user_name > /dev/null 2>&1; then
+    echo "$user_name:$pssword" | chpasswd
+    echo -e "${BCG}Successfully changed password on $i ${EC}"
+    exit 1
+else
+    echo "${BC}$user_name, user not found in $i${EC}"
+fi
+EOF
+done
+fi
