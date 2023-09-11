d94a08523456f677508378999e53116d4dda0c92
rahul-wayne <58177891+rahul-wayne@users.noreply.github.com>
2023-09-11 22:30:31 +0530

    Update time_zone_convert.sh

--- time_zone_convert.sh
+++ time_zone_convert.sh
@@ -11,27 +11,33 @@
 echo -e "\n"
 val=0
 current_time=$(date "+%Y-%m-%d %H:%M %Z")
-echo Current System time is $current_time 
+echo Current System time is $current_time
 echo -e "\n"
 while [[ $val == 0 ]]; do
 BC=$'\033[1;31m'
 EC=$'\033[0m'
 read -r -p  "Enter the date and time in YYYY-MM-DD HH:MM (24Hr) [${BC}IN ANY TIME ZONE${EC}]  format eg: 2023-07-24 14:00 IST:"  inp
-#echo -e "\n"
-if [ -n "$inp" ] 
+
+if [ -n "$inp" ]
 then
+
+CTZ=`date | awk '{print $5}'`
+GTZ=`echo $inp | awk '{print $3}'`
+tr_inp=`echo $inp | awk '{print $1,$2}'`
+FTZ=`date -d "$tr_inp" | awk '{print $5}'`
+
+if [[ "$GTZ" == "EST" || "$GTZ" == "EDT" ]];then
+if [[ "$GTZ" != $FTZ ]];then
+ echo "You have entered wrong time zone. Please check"
+ echo xxxxx Actual Time Zone: $FTZ xxxxx
+ echo Given Time: $tr_inp
+ echo Current Time Zone is: $CTZ
+ echo Given Time Zone is: $GTZ
+ exit
+fi
+fi
 BST=`TZ="Europe/London" date -d "$inp" "+%Y-%m-%d %H:%M %Z"`
 #echo $BST
-#echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-#echo "Given time $inp in different Time Zones"
-#echo "********************************************************"
-#TZ="Europe/London" date -d "$inp" "+%Y-%m-%d %H:%M %Z"
-#TZ="UTC" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
-#TZ="America/Costa_Rica" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
-#TZ="America/Chicago" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
-#TZ="EST" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
-#TZ="America/New_York" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
-#TZ="Asia/Kolkata" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
 if [[ $? -ne 0 ]]
 then
   echo "**** ERROR :: INVALID DATE ****"
@@ -57,6 +63,7 @@ else
     TZ="Asia/Kolkata" date -d "$BST" "+%Y-%m-%d %H:%M %Z"
   fi
   echo "########################################################"
+  echo -e "\n"
   echo "Daylight Saving Time (DST) is the practice of advancing clocks during summer months by one hour"
   echo  DST - \"From Second Sunday in March to the First Sunday in November\"
 fi
@@ -64,4 +71,3 @@ else
  val=0
 fi
 done
-
